bd0f8ae587ff6a34e7a333c484673c94cadf3ddf
GrantMeehan <46740377+GrantMeehan@users.noreply.github.com>
2022-03-15 15:07:44 -0700

    Added Pest class and PestController and began outlining functional purpose

---
+++ src/main/java/com/example/automatedgarden/Pest.java
@@ -0,0 +1,4 @@
+package com.example.automatedgarden;
+
+public class Pest {
+}
---
+++ src/main/java/com/example/automatedgarden/PestController.java
@@ -0,0 +1,21 @@
+package com.example.automatedgarden;
+
+public class PestController {
+
+    public static int pestSpawnTime; //random number between 0 (inclusive) and 6 (exclusive) OR 20 (inclusive) and 24 (exclusive)
+
+    //randomly generate number of pests that will spawn (1 - # of total plants?)
+    //randomly generate pestSpawnTime using parameters given above
+    //loop through plant2DArray. if plant exists, randomly decide if pest should spawn
+    //if pest will spawn, create new pest object at same coordinates of plant in pest2DArray
+    //update image to reflect
+    public static void generatePests() {
+
+    }
+
+    //set all objects in pest2DArray to null
+    //update images on grid to reflect
+    public static void killPests() {
+
+    }
+}
